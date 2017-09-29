790484954646668bb5a90723297554c3048a9b16
Смирнов Владимир Александрович <vladimir_smirnov@epam.com>
2017-09-30 00:39:16 +0300

    Task 3 fix

--- Task1/src/index.jsx
+++ Task1/src/index.jsx
@@ -4,7 +4,6 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { App } from './App';
 import { NotFound } from './components/NotFound';
 import { SearchPage } from './components/SearchPage';
-import { Welcome } from './components/Welcome';
 
 ReactDom.render((
     <Router>
@@ -12,7 +11,7 @@ ReactDom.render((
             <Switch>
                 <Route exact path="/" component={SearchPage} />
                 <Route path="/film/:id" component={SearchPage} />
-                <Route path="/search/:searchBy/:searchText/:sortBy" component={SearchPage} />               
+                <Route path="/search/:searchBy/:searchText/:sortBy" component={SearchPage}/>               
                 <Route path="*" component={NotFound} />
 
             </Switch>
